bad7694abc1cd3368b0cdd0e583d4ae1935afded
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2014-07-09 14:56:51 -0400

    Added graphing tools, not working

--- src/cms/view/panel/GraphingPanel.java
+++ src/cms/view/panel/GraphingPanel.java
@@ -1,14 +1,32 @@
 package cms.view.panel;
 
-import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
+import cms.controller.LogSystem;
+import cms.model.DataFactory;
 import cms.view.GraphicsConstants;
+import cms.view.element.GraphBar;
 
 public class GraphingPanel extends JPanel implements GraphicsConstants{
+	
+	private static GraphBar[] graphbar;
+	private Dimension size;
+	private int barcount;
 
 	public GraphingPanel(){
-		this.setBackground(PRE);
+		this.setBackground(ABS);
+		this.barcount = DataFactory.core.length;
+		//LogSystem.log(true, false, ""+this.);
+		graphbar = new GraphBar[barcount];
+		
+		for(int i = 0; i < barcount; i++){
+			graphbar[i] = new GraphBar(this.getWidth()/barcount, DataFactory.core[i]);
+			this.add("Bar", graphbar[i]);
+			LogSystem.log(true, false, ""+ this.getSize());
+			//LogSystem.log(true, false, "Created bar no." +i);
+		}
 	}
+	
 }
