a210febefae294c05081d65e457444a44de6aeec
Vynny <namuknoble@gmail.com>
2014-07-29 17:06:41 -0400

    fixed server, implementing temp sensing for XML test

--- src/cms/model/communication/SClientSession.java
+++ src/cms/model/communication/SClientSession.java
@@ -1,5 +1,50 @@
 package cms.model.communication;
 
-public class SClientSession {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+import cms.controller.LogSystem;
+import cms.helpers.ThreadHelper;
+
+public class SClientSession extends ThreadHelper {
+
+	protected Socket clientConnectionSocket = null;
+	protected String serverTransmission = null;
+	protected String serverInput = null;
+
+	public SClientSession(Socket clientConnectionSocket, String serverTransmission) {
+		this.clientConnectionSocket = clientConnectionSocket;
+		this.serverTransmission = serverTransmission;
+	}
+
+	public void run() {
+		try {
+			System.out.println("STARTING CLIENT SESSION");
+			BufferedReader input =  new BufferedReader(new InputStreamReader(
+					clientConnectionSocket.getInputStream()));
+			PrintWriter output =  new PrintWriter(
+					clientConnectionSocket.getOutputStream(), true);
+			
+			long time = System.currentTimeMillis();
+			
+			serverInput = input.readLine();
+			LogSystem.log(true, false, "Read line.");
+			System.out.println("inputL: " + serverInput);
+			LogSystem.log(true, false, "Response from Client("
+					+ clientConnectionSocket.getInetAddress()
+							.getHostAddress() + ": " + serverInput + "(BYTES: "
+					+ serverInput.getBytes().length + ")");
+			output.println("You said: " + serverInput);
+			
+			output.close();
+			input.close();
+			//System.out.println("Request processed: " + time);
+		} catch (IOException e) {
+			// report exception somewhere.
+			e.printStackTrace();
+		}
+	}
 }
--- src/cms/model/communication/Server.java
+++ src/cms/model/communication/Server.java
@@ -2,23 +2,26 @@ package cms.model.communication;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import cms.controller.LogSystem;
 import cms.helpers.ThreadHelper;
 
 public class Server extends ThreadHelper {
-	public PrintWriter output;
-	public BufferedReader input;
-	public ServerSocket serverSocket;
-	public Socket clientConnectionSocket;
-
 	public boolean SERVER_RUNNING = false;
-	private int port, backlog;
-	private boolean NOCONN = true;
+	public int MAX_SERVER_THREAD_POOLS = 10;
+	
+	protected int port, backlog;
+	
+	protected PrintWriter output;
+	protected BufferedReader input;
+	protected ServerSocket serverSocket;
+	protected Socket clientConnectionSocket;
+	protected ExecutorService threadPool;
 
 	public Server(int port, int backlog) {
 		this.serverSocket = null;
@@ -26,6 +29,7 @@ public class Server extends ThreadHelper {
 		this.port = port;
 		this.backlog = backlog;
 		this.SERVER_RUNNING = true;
+		threadPool = Executors.newFixedThreadPool(MAX_SERVER_THREAD_POOLS);
 	}
 
 	public void startRunning() throws ClassNotFoundException {
@@ -38,66 +42,19 @@ public class Server extends ThreadHelper {
 
 			try {
 				clientConnectionSocket = this.serverSocket.accept();
-				if (clientConnectionSocket != null) {
-					break;
-				}
 			} catch (NullPointerException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
-			NOCONN = false;
-
-			if (clientConnectionSocket.isConnected() && !NOCONN) {
-				System.out.println("Client connected on port "
-						+ clientConnectionSocket.getPort());
-
-				output = new PrintWriter(
-						clientConnectionSocket.getOutputStream(), true);
-				LogSystem.log(true, false, "Started output stream buffer: "
-						+ output.toString());
-				// output.flush();
-				input = new BufferedReader(new InputStreamReader(
-						clientConnectionSocket.getInputStream()));
-				LogSystem.log(true, false, "Started input stream buffer: "
-						+ input.toString());
-				// output.flush();
+			//Create a thread for a client if accepted containing an SClientSession
+			this.threadPool.execute(new Thread(new SClientSession(clientConnectionSocket, "Multithreaded Server")));
+			
 			}
-
-			// }
-			/*
-			 * LOGIC
-			 */
-			String inputL = "";
-
-			int c = 0;
-			if (c == 0)
-				LogSystem.log(true, false, "Starting communication..");
-
-			do {
-				if (clientConnectionSocket.isClosed())
-					break;
-				c = 1;
-				inputL = input.readLine();
-				LogSystem.log(true, false, "Read line.");
-				System.out.println("inputL: " + inputL);
-				LogSystem.log(true, false, "Response from Client("
-						+ clientConnectionSocket.getInetAddress()
-								.getHostAddress() + ": " + inputL + "(BYTES: "
-						+ inputL.getBytes().length + ")");
-				output.println("You said: " + inputL);
-
-				Thread.sleep(100);
-			} while (inputL != null);
-
-			LogSystem.log(true, false, "Client "
-					+ clientConnectionSocket.getInetAddress().getHostAddress()
-					+ " has stopped communicating");
-			NOCONN = true;
-
-		}
+		
+		LogSystem.log(true, false, "Server has stopped running");
+		System.out.println("Server no longer listening on port: " + port);
 	}
 
 	private void openServerSocket(int port) {
@@ -112,26 +69,58 @@ public class Server extends ThreadHelper {
 	@Override
 	public void run() {
 		try {
-			// while (true) {
 			startRunning();
-			// Thread.sleep(50);
-			// }
-			// startRunning();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void start() {
-		try {
-			serverSocket = new ServerSocket(port, backlog);
-			clientConnectionSocket = new Socket();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			super.start();
-		}
-	}
-
 }
+
+/*if (clientConnectionSocket.isConnected() && !NOCONN) {
+System.out.println("Client connected on port "
+		+ clientConnectionSocket.getPort());
+
+output = new PrintWriter(
+		clientConnectionSocket.getOutputStream(), true);
+LogSystem.log(true, false, "Started output stream buffer: "
+		+ output.toString());
+// output.flush();
+input = new BufferedReader(new InputStreamReader(
+		clientConnectionSocket.getInputStream()));
+LogSystem.log(true, false, "Started input stream buffer: "
+		+ input.toString());
+// output.flush();
+}*/
+
+// }
+/*
+* LOGIC
+*/
+/*String inputL = "";
+
+int c = 0;
+if (c == 0)
+LogSystem.log(true, false, "Starting communication..");
+
+do {
+if (clientConnectionSocket.isClosed())
+	break;
+c = 1;
+inputL = input.readLine();
+LogSystem.log(true, false, "Read line.");
+System.out.println("inputL: " + inputL);
+LogSystem.log(true, false, "Response from Client("
+		+ clientConnectionSocket.getInetAddress()
+				.getHostAddress() + ": " + inputL + "(BYTES: "
+		+ inputL.getBytes().length + ")");
+output.println("You said: " + inputL);
+
+Thread.sleep(100);
+} while (inputL != null);
+
+LogSystem.log(true, false, "Client "
+	+ clientConnectionSocket.getInetAddress().getHostAddress()
+	+ " has stopped communicating");
+NOCONN = true;*/
+
