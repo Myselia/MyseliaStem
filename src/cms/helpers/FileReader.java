c990b0450d4c5d7728cfdbc4dfe83de4a122e4f8
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2014-07-29 19:18:36 -0400

    Added file reader class

---
+++ src/cms/helpers/FileReader.java
@@ -0,0 +1,27 @@
+package cms.helpers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+class FileReaderZ {
+
+	/*public static BufferedReader getBufferedReader() {
+		
+		File f = new File("res\\transmissionprototype.xml");
+		FileReader fr;
+		BufferedReader br;
+		
+		try {
+			fr = new FileReader(f);
+			br  = new BufferedReader(fr);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return br;
+ 
+	}*/
+}
