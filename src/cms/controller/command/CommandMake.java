bd942fd6cae0e41550aa8604963cab1f2b6ac980
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2014-08-03 11:39:55 -0400

    added command MAKE to set type to node

---
+++ src/cms/controller/command/CommandMake.java
@@ -0,0 +1,48 @@
+package cms.controller.command;
+
+import cms.model.DataFactory;
+import cms.view.AddressBar;
+
+public class CommandMake extends AbstractCommand{
+	private final static String command_signature = "make";
+	
+	@Override
+	public void action(String arg){
+		String[] parameters = super.commandParam(arg);
+		if(parameters.length > 1){
+			if(parameters[1].equals("def")){
+				define();
+			}else if(parameters[1].equals("node")){
+				make(0);
+			}else if(parameters[1].equals("CMS")){
+				make(1);
+			}else if(parameters[1].equals("AMS")){
+				make(2);
+			}else if(parameters[1].equals("DB")){
+				make(3);
+			}else {
+				System.out.println("e>" + "Not Supported");
+			}
+		}else{
+			System.out.println("e>" + "Command Incomplete");
+		}		
+	}
+	
+	private void make(int type){
+		int coreid = DataFactory.getSelectedCore();
+		DataFactory.core[coreid].setType(type);
+		AddressBar.nodeButton(coreid).repaint();
+	}
+
+	@Override
+	public void define() {
+		System.out.println("Changes the type of a node in the cluster.");
+		System.out.println("Parameters: 'this', 'AMS', or 'DB'.");
+	}
+	
+	@Override
+	public String getCommandSignature(){
+		return CommandMake.command_signature;
+	}
+
+}
