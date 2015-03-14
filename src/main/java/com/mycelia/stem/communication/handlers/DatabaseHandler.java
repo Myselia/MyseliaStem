b5725740f4f23dfddd39f048cb846ceedbc9aa8c
Eduard Paraschivescu <ed.orangeduke@gmail.com>
2015-03-14 12:06:38 -0400

    Handlers for the client sessions

---
+++ src/main/java/com/mycelia/stem/communication/handlers/DatabaseHandler.java
@@ -0,0 +1,5 @@
+package com.mycelia.stem.communication.handlers;
+
+public class DatabaseHandler {
+
+}
---
+++ src/main/java/com/mycelia/stem/communication/handlers/Handler.java
@@ -0,0 +1,8 @@
+package com.mycelia.stem.communication.handlers;
+
+public abstract class Handler {
+	String ip = "";
+	
+	
+
+}
